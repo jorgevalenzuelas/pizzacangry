a837cb4e69027bdfab4653bd30229185d8535b31
jorge <jorge_vs@hotmail.es>
2021-04-30 04:42:41 -0500

    msj

--- app/vistas/chat/Chat.php
+++ app/vistas/chat/Chat.php
@@ -9,7 +9,7 @@
 
     <script>
         $(document).ready(function (e){
-            var conn = new WebSocket('wss://www.pizzaloscangry.tk/phpchat');//conectara
+            var conn = new WebSocket('wss://www.pizzaloscangry.tk/phpchat/');//conectara
 
             conn.onopen = function(e) {
                 console.log("conexion exitosa!");
