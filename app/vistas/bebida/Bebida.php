0b6d6511e2e14604f9fe7c623dd2074ffe0e1759
jorge <jorge_vs@hotmail.es>
2021-05-05 18:48:33 -0500

    agrege un comentario en linea 0

--- app/vistas/bebida/Bebida.php
+++ app/vistas/bebida/Bebida.php
@@ -1,7 +1,9 @@
 
+<!--comenta-->
 <!DOCTYPE html>
 <html>
 <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?php echo NOMBRE_SITIO; ?> | Bebidas</title>
